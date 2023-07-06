aa054ba6619d1a3f4ee1f294d81d1f4a664e9e19
Jonathan Bijl <sannoayo@gmail.com>
2023-07-05 21:15:31 -0400

    Finalize React Ironcontact project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import contacts from "./contacts.json";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const initialContacts = contacts.slice(0, 5);
+  const [contactList, setContactList] = useState(initialContacts);
+
+  const addRandomContact = () => {
+    const remainingContacts = contacts.filter(
+      (contact) => !contactList.includes(contact)
+    );
+
+    if (remainingContacts.length > 0) {
+      const randomIndex = Math.floor(Math.random() * remainingContacts.length);
+      const randomContact = remainingContacts[randomIndex];
+
+      setContactList((prevList) => [...prevList, randomContact]);
+    }
+  };
+
+  const sortByName = () => {
+    const sortedContacts = contactList.slice().sort((a, b) =>
+      a.name.localeCompare(b.name)
+    );
+    setContactList(sortedContacts);
+  };
+
+  const sortByPopularity = () => {
+    const sortedContacts = contactList
+      .slice()
+      .sort((a, b) => b.popularity - a.popularity);
+    setContactList(sortedContacts);
+  };
+
+  const deleteContact = (id) => {
+    const updatedList = contactList.filter((contact) => contact.id !== id);
+    setContactList(updatedList);
+  };
+
+  const imgStyle = {
+    width: "50px",
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>Contacts</h1>
+        <div className="buttons">
+          <button onClick={addRandomContact}>Add Random Contact</button>
+          <button onClick={sortByName}>Sort by Name</button>
+          <button onClick={sortByPopularity}>Sort by Popularity</button>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Won an Oscar</th>
+              <th>Won an Emmy</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {contactList.map((contact) => (
+              <tr key={contact.id}>
+                <td>
+                  <img src={contact.pictureUrl} alt={contact.name} style={imgStyle} />
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity}</td>
+                <td>{contact.wonOscar && "🏆"}</td>
+                <td>{contact.wonEmmy && "🏆"}</td>
+                <td>
+                  <button onClick={() => deleteContact(contact.id)}>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
